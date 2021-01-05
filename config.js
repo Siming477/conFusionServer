92ce6b9e4a04b962cbe542ab7d1b0e7b914b2611
siming477 <hsim47@163.com>
2021-01-04 23:51:29 -0500

    OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '189151672916499',
+        clientSecret: '50a3cdbc2cefe43e37c6bd7749ffd76d'
+    }
 }
